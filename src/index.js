c4822906dd006a7ee54e6266db1b1fe9ac87f654
KaipoJames <kaiposemail@yahoo.com>
2020-12-21 08:52:25 -1000

    Revert to hardcode header on every all html pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const logo = document.querySelector(".logo");
+const logo = document.querySelector(".homeBtn");
 
 if (logo) {
     logo.addEventListener("click", () => { window.location.href = "http://127.0.0.1:5500/src/index.html"; console.log("clicked logo"); });
