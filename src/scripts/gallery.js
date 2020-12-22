f9c4cdc3f4be06cb3ba38df549d7389f51dd2079
KaipoJames <kaiposemail@yahoo.com>
2020-12-21 21:59:04 -1000

    add image slider to gallery.html

---
+++ src/scripts/gallery.js
@@ -0,0 +1,54 @@
+const dots = document.querySelectorAll(".dot");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+let slideIndex = 1;
+
+const displaySlides = (index) => {
+    let i;
+    const dots = document.querySelectorAll(".dot");
+    const slides = document.querySelectorAll(".slide");
+    if (index > slides.length) {
+        slideIndex = 1;
+    }
+    if (index < 1) {
+        slideIndex = slides.length;
+    } 
+    console.log("Index: " + index);
+    console.log("SlideIndex: " + slideIndex);
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    dots[slideIndex-1].classList.add("active");
+    slides[slideIndex-1].style.display = "block";
+}
+
+const currentSlide = (id) => {
+    displaySlides(slideIndex = id);
+}
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", () => {
+        let id = parseInt(dots[i].getAttribute("id"));
+        currentSlide(id);
+    });
+}
+
+if (prevBtn) {
+    prevBtn.addEventListener("click", () => {
+        slideIndex--;
+        displaySlides(slideIndex);
+    });
+}
+if (nextBtn) {
+    nextBtn.addEventListener("click", () => {
+        slideIndex++;
+        displaySlides(slideIndex);
+    });
+}
+
+displaySlides(slideIndex);
